01ccb7591f312624e13a46c504a9c544a4afd32f
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-04-09 17:02:45 -0400

    Add container of thing

--- include/common/ares/util.hpp
+++ include/common/ares/util.hpp
@@ -192,4 +192,32 @@ py::dict to_dict(Args &&...args) {
                 (Z_NV_CONTAINER(field_, container_, false)),                   \
                 (Z_NV(field_, false)))
 
+/**
+ * Get a pointer to a structure containing the element.
+ *
+ * Example:
+ *
+ * ```c
+ * struct Foo {
+ *     int bar;
+ * }
+ *
+ * Foo my_foo;
+ * int *ptr = &my_foo.bar;
+ *
+ * Foo *container = container_of(ptr, &Foo::bar);
+ * ```
+ *
+ * @tparam Parent The parent type.
+ * @tparam Member The data member type.
+ * @param ptr Pointer to the data member.
+ * @param member_ptr Pointer to the field the data member points to.
+ * @return Pointer to the parent object.
+ */
+template<typename Parent, typename Member>
+Parent *container_of(Member *ptr, Member Parent::*member_ptr) {
+    size_t offset = reinterpret_cast<size_t>(&(static_cast<Parent*>(nullptr)->*member_ptr));
+    return reinterpret_cast<Parent *>(reinterpret_cast<char *>(ptr) - offset);
+}
+
 #endif // ARES_UTIL_HPP
