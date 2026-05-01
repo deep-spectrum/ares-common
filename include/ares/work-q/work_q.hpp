64ef2cc977c12f5e0ebce9f682b6a4fdc1e9181a
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-01 15:47:46 -0400

    Write docs for work queue

--- include/ares/work-q/work_q.hpp
+++ include/ares/work-q/work_q.hpp
@@ -164,33 +164,192 @@ struct WorkDelayable {
 };
 #endif // DELAYABLE_WORK
 
+/**
+ * @struct WorkQConfig
+ * @brief A structure holding optional configuration items for a work queue.
+ *
+ * This structure, and values it references, are not retained by WorkQ::start
+ */
 struct WorkQConfig {
+    /**
+     * @brief The name to be given to the work queue thread.
+     *
+     * If left null the thread will not have a name.
+     */
     const char *name = "";
+
+    /**
+     * Control whether the work queue thread should yield between items.
+     *
+     * Yielding between items helps guarantee the work queue thread does not
+     * starve other threads, including cooperative ones released by a work item.
+     * This is the default behavior.
+     *
+     * Set this to `true` to prevent the work queue thread from yielding between
+     * items. This may be appropriate when a sequence of items should complete
+     * without yielding control.
+     */
     bool no_yield = false;
+
+    /**
+     * @brief Control whether the work queue thread should be marked as
+     * essential thread.
+     */
     bool essential = false;
 
-    // unused right now
+    /**
+     * @brief Controls whether work queue monitors work timeouts.
+     *
+     * If non-zero, the work queue will monitor the duration of each work item.
+     * If the work item handler takes longer than the specified time to execute,
+     * the work queue thread will be aborted, and an error will be logged.
+     *
+     * @note This behavior has not been implemented yet.
+     * @todo Implement this.
+     */
     std::chrono::milliseconds work_timeout_ms = 0ms;
 };
 
+/**
+ * @class WorkQ
+ * @brief A structure used to submit work.
+ */
 class WorkQ {
   public:
+    /**
+     * Constructor.
+     */
     WorkQ();
+
+    /**
+     * Destructor.
+     */
     ~WorkQ();
 
+    /**
+     * @brief Submit a work item to a queue.
+     *
+     * @param[in] work Pointer to the work item.
+     *
+     * @return 0 if work was already submitted to a queue.
+     * @return 1 if work was not submitted and has been queued.
+     * @return 2 if work was running and has been queue to the queue that was
+     * running it.
+     * @return -EBUSY
+     * * if work submission was rejected because the work item is cancelling; or
+     * * queue is draining; or
+     * * queue is plugged.
+     * @return -ENODEV if queue has not started.
+     */
     int submit(Work *work);
+
+    /**
+     * @brief Initializes work queue.
+     *
+     * This configures the work queue thread and starts it running. This
+     * function should not be re-invoked on a queue.
+     *
+     * @param[in] config Optional configuration parameters. Pass @c nullptr if
+     * not required, use the defaults documented in WorkQConfig.
+     */
     void start(const WorkQConfig *config);
+
+    /**
+     * @brief Run work queue using calling thread.
+     *
+     * This will run the work queue forever unless stopped by WorkQ::stop().
+     * This function should not be invoked from a queue.
+     *
+     * @param[in] config Optional configuration parameters. Pass @c nullptr if
+     * not required, use the defaults documented in WorkQConfig.
+     */
     void run(const WorkQConfig *config);
+
+    /**
+     * @brief Access the thread ID that animates a work queue.
+     *
+     * This is necessary to grant a work queue thread access to things the work
+     * items it will process are expected to use.
+     *
+     * @return the thread ID associated with the work queue.
+     */
     [[nodiscard]] std::thread::id queue_thread_get() const;
+
+    /**
+     * @brief Wait until the work queue has drained, optionally plugging it.
+     *
+     * This blocks submission to the work queue except when coming from queue
+     * thread, and blocks the caller until no more work items are available in
+     * the queue.
+     *
+     * If @p plug is true then submission will continue to be blocked after the
+     * drain operation completes until WorkQ::queue_unplug() is invoked.
+     *
+     * @param[in] plug if `true`, the work queue will continue to block new
+     * submissions after all items have drained.
+     * @return 1 if the caller had to wait for the drain to complete.
+     * @return 0 if call did not have to wait.
+     * @return negative error code if wait was interrupted or failed.
+     *
+     * @note Work items that are delayed are not yet associated with their
+     * work queue.  They must be canceled externally if a goal is to ensure the
+     * work queue remains empty.  The @p plug feature can be used to prevent
+     * delayed items from being submitted after the drain completes.
+     */
     int queue_drain(bool plug);
+
+    /**
+     * @brief Release a work queue to accept new submissions.
+     *
+     * This releases the block on new submissions placed when
+     * WorkQ::queue_drain() is invoked with the @p plug option enabled.  If this
+     * is invoked before the drain completes new items may be submitted as soon
+     * as the drain completes.
+     *
+     * @return 0 if successfully unplugged.
+     * @return -EALREADY if the work queue was not plugged.
+     */
     int queue_unplug();
+
+    /**
+     * @brief Stop the work queue.
+     *
+     * Stops the work queue thread and ensures that no further work will be
+     * processed. This call is blocking and guarantees that the work queue
+     * thread has terminated cleanly if successful, no work will be processed
+     * past this point.
+     *
+     * @return 0 if the work queue was stopped.
+     * @return -EALREADY if the work queue was not started (or already stopped)
+     * @return -EBUSY if the work queue is actively processing work items
+     */
     int stop();
-    int stop(std::chrono::milliseconds timeout);
+
+    /**
+     * @brief Stop the work queue.
+     *
+     * Stops the work queue thread and ensures that no further work will be
+     * processed. This call is blocking and guarantees that the work queue
+     * thread has terminated cleanly if successful, no work will be processed
+     * past this point.
+     *
+     * @param[in] timeout Maximum time to wait for the work queue to stop.
+     *
+     * @return 0 if the work queue was stopped.
+     * @return -EALREADY if the work queue was not started (or already stopped)
+     * @return -EBUSY if the work queue is actively processing work items
+     */
+    int stop(const std::chrono::milliseconds &timeout);
 #if DELAYABLE_WORK
     int schedule(WorkDelayable *dwork, std::chrono::milliseconds delay);
     int reschedule(WorkDelayable *dwork, std::chrono::milliseconds delay);
 #endif // DELAYABLE_WORK
 
+    /**
+     * @brief Check if the work queue is plugged.
+     * @return `true` if the queue is plugged and not accepting new work items,
+     * `false` otherwise.
+     */
     [[nodiscard]] bool plugged() const;
 
     friend struct Work;
@@ -232,6 +391,24 @@ int work_reschedule(WorkDelayable *dwork, std::chrono::milliseconds delay);
 #endif // DELAYABLE_WORK
 #endif // SYS_WORK_QUEUE
 
+/** @brief Submit a work item to a queue.
+ *
+ * @param queue pointer to the work queue on which the item should run.  If
+ * NULL the queue from the most recent submission will be used.
+ *
+ * @param work pointer to the work item.
+ *
+ * @retval 0 if work was already submitted to a queue
+ * @retval 1 if work was not submitted and has been queued to @p queue
+ * @retval 2 if work was running and has been queued to the queue that was
+ * running it
+ * @retval -EBUSY
+ * * if work submission was rejected because the work item is cancelling; or
+ * * @p queue is draining; or
+ * * @p queue is plugged.
+ * @retval -EINVAL if @p queue is null and the work item has never been run.
+ * @retval -ENODEV if @p queue has not been started.
+ */
 int work_submit_to_queue(WorkQ *queue, Work *work);
 #if DELAYABLE_WORK
 int work_schedule_for_queue(WorkQ *queue, WorkDelayable *dwork,
