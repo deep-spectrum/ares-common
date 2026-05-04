013427046f035734737fb9f09372d8ca5f45ddfd
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-04 14:11:34 -0400

    Write is empty test case

--- tests/ares-common/src/macro_test.cpp
+++ tests/ares-common/src/macro_test.cpp
@@ -8,8 +8,8 @@
  * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
  */
 
-#include <gtest/gtest.h>
 #include <ares/util.h>
+#include <gtest/gtest.h>
 
 #define FOO 17
 #define BAR 23
@@ -19,10 +19,9 @@ TEST(AresUtils, Stringify) {
     EXPECT_STREQ(MACRO_STRINGIFY(BAR), "23");
 }
 
+TEST(AresUtils, ConditionCodeMacros) {
 #define FLAG_0 0
 #define FLAG_1 1
-
-TEST(AresUtils, ConditionCodeMacros) {
     int val;
 
     COND_CODE_0(FLAG_0, (val = FOO), (val = BAR));
@@ -36,4 +35,15 @@ TEST(AresUtils, ConditionCodeMacros) {
 
     COND_CODE_1(FLAG_1, (val = FOO), (val = BAR));
     EXPECT_EQ(val, FOO);
+}
+
+TEST(AresUtils, IsEmpty) {
+#define test_IS_EMPTY_REAL_EMPTY
+#define test_IS_EMPTY_NOT_EMPTY XXX_DO_NOT_REPLACE_XXX
+    int foo = 0;
+    EXPECT_TRUE(IS_EMPTY(test_IS_EMPTY_REAL_EMPTY));
+    EXPECT_FALSE(IS_EMPTY(test_IS_EMPTY_NOT_EMPTY));
+    EXPECT_FALSE(IS_EMPTY("string"));
+    EXPECT_FALSE(IS_EMPTY(&foo));
+    ARG_UNUSED(foo);
 }
