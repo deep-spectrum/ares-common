5081efab2c80d8d7f5e2f9be3ce5522516a64f6f
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-04 16:55:44 -0400

    Write python tests

---
+++ tests/ares-common/pyutils/src/pyutils_test_core/__init__.py
@@ -0,0 +1,12 @@
+from ._pyutils_test import static_c_array_tuple, dynamic_c_array_tuple, dynamic_cpp_array_tuple, cpp_array, cpp_vector, \
+    C_ARRAY_SIZE, CPP_ARRAY_SIZE
+
+__all__ = [
+    "static_c_array_tuple",
+    "dynamic_c_array_tuple",
+    "dynamic_cpp_array_tuple",
+    "cpp_array",
+    "cpp_vector",
+    "C_ARRAY_SIZE",
+    "CPP_ARRAY_SIZE",
+]
---
+++ tests/ares-common/pyutils/tests/test_array_to_tuple.py
@@ -0,0 +1,47 @@
+from pyutils_test_core import static_c_array_tuple, dynamic_c_array_tuple, dynamic_cpp_array_tuple, cpp_array, \
+    cpp_vector, C_ARRAY_SIZE, CPP_ARRAY_SIZE
+import random
+import pytest
+
+
+@pytest.mark.parametrize('execution_number', range(10))
+def test_static_c_array(execution_number):
+    start = random.randint(-100, 100)
+    expected = tuple([int(x) for x in range(start, start + C_ARRAY_SIZE, 1)])
+    result = static_c_array_tuple(start)
+    assert expected == result
+
+
+@pytest.mark.parametrize('execution_number', range(10))
+def test_dynamic_c_array(execution_number):
+    start = random.randint(-100, 100)
+    size = random.randint(2, 10000)
+    expected = tuple([int(x) for x in range(start, start + size, 1)])
+    result = dynamic_c_array_tuple(start, size)
+    assert expected == result
+
+
+@pytest.mark.parametrize('execution_number', range(10))
+def test_dynamic_cpp_array(execution_number):
+    start = random.randint(-100, 100)
+    size = random.randint(2, 10000)
+    expected = tuple([int(x) for x in range(start, start + size, 1)])
+    result = dynamic_cpp_array_tuple(start, size)
+    assert expected == result
+
+
+@pytest.mark.parametrize('execution_number', range(10))
+def test_cpp_array(execution_number):
+    start = random.randint(-100, 100)
+    expected = tuple([int(x) for x in range(start, start + CPP_ARRAY_SIZE, 1)])
+    result = cpp_array(start)
+    assert expected == result
+
+
+@pytest.mark.parametrize('execution_number', range(10))
+def test_cpp_vector(execution_number):
+    start = random.randint(-100, 100)
+    size = random.randint(2, 10000)
+    expected = tuple([int(x) for x in range(start, start + size, 1)])
+    result = cpp_vector(start, size)
+    assert expected == result
