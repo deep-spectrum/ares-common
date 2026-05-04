95278f1b2b1acd5988bfcec2b1410daf1846744d
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-04 12:24:39 -0400

    Add condition code tests

--- tests/ares-common/src/macro_test.cpp
+++ tests/ares-common/src/macro_test.cpp
@@ -18,3 +18,22 @@ TEST(AresUtils, Stringify) {
     EXPECT_STREQ(STRINGIFY(FOO), "FOO");
     EXPECT_STREQ(MACRO_STRINGIFY(BAR), "23");
 }
+
+#define FLAG_0 0
+#define FLAG_1 1
+
+TEST(AresUtils, ConditionCodeMacros) {
+    int val;
+
+    COND_CODE_0(FLAG_0, (val = FOO), (val = BAR));
+    EXPECT_EQ(val, FOO);
+
+    COND_CODE_0(FLAG_1, (val = FOO), (val = BAR));
+    EXPECT_EQ(val, BAR);
+
+    COND_CODE_1(FLAG_0, (val = FOO), (val = BAR));
+    EXPECT_EQ(val, BAR);
+
+    COND_CODE_1(FLAG_1, (val = FOO), (val = BAR));
+    EXPECT_EQ(val, FOO);
+}
