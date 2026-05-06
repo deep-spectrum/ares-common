a7525b7addb1e2ca2d8dda70c33b64770fd684f7
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-06 12:00:12 -0400

    Write basic dict tests part2

--- tests/ares-common/pyutils/tests/test_to_dict.py
+++ tests/ares-common/pyutils/tests/test_to_dict.py
@@ -107,3 +107,108 @@ def test_basic_dict2_individual_drop_value():
     cut = ToDictTest(expected["a"], expected["b"], expected["c"], expected["d"], expected["e"])
     result = cut.basic_dict2()
     assert expected == result
+
+
+def test_basic_dict3_random_values():
+    expected = {
+        "a": random.randint(-65535, 65535),
+        "b": random.randint(-65535, 65535),
+        "c": random.randint(-65535, 65535),
+    }
+    cut = ToDictTest(expected["a"], expected["b"], expected["c"], 0, 0)
+    result = cut.basic_dict3()
+    assert expected == result
+
+def test_basic_dict3_common_drop_value():
+    # Test that no common drop checks are taking place
+    expected = {
+        "a": TODICT_COMMON_DROP_VAL,
+        "b": TODICT_COMMON_DROP_VAL,
+        "c": TODICT_COMMON_DROP_VAL,
+    }
+    cut = ToDictTest(expected["a"], expected["b"], expected["c"], 0, 0)
+    result = cut.basic_dict3()
+    assert expected == result
+
+
+def test_basic_dict3_individual_drop_value():
+    # Test that no drop checks are taking place
+    expected = {
+        "a": TODICT_DROP_A_VAL,
+        "b": TODICT_DROP_B_VAL,
+        "c": TODICT_DROP_C_VAL,
+    }
+    cut = ToDictTest(expected["a"], expected["b"], expected["c"], 0, 0)
+    result = cut.basic_dict3()
+    assert expected == result
+
+
+def test_basic_dict4_random_values():
+    expected = {
+        "d": random.randint(-65535, 65535),
+        "e": random.randint(-65535, 65535),
+    }
+    cut = ToDictTest(0, 0, 0, expected["d"], expected["e"])
+    result = cut.basic_dict4()
+    assert expected == result
+
+def test_basic_dict4_common_drop_value():
+    # Test that no common drop checks are taking place
+    expected = {
+        "d": TODICT_COMMON_DROP_VAL,
+        "e": TODICT_COMMON_DROP_VAL,
+    }
+    cut = ToDictTest(0, 0, 0, expected["d"], expected["e"])
+    result = cut.basic_dict4()
+    assert expected == result
+
+
+def test_basic_dict4_individual_drop_value():
+    # Test that no drop checks are taking place
+    expected = {
+        "d": TODICT_DROP_D_VAL,
+        "e": TODICT_DROP_E_VAL,
+    }
+    cut = ToDictTest(0, 0, 0, expected["d"], expected["e"])
+    result = cut.basic_dict4()
+    assert expected == result
+
+
+def test_basic_dict5_random_values():
+    expected = {
+        "a": random.randint(-65535, 65535),
+        "b": random.randint(-65535, 65535),
+        "c": random.randint(-65535, 65535),
+        "d": random.randint(-65535, 65535),
+        "e": random.randint(-65535, 65535),
+    }
+    cut = ToDictTest(expected["a"], expected["b"], expected["c"], expected["d"], expected["e"])
+    result = cut.basic_dict5()
+    assert expected == result
+
+def test_basic_dict5_common_drop_value():
+    # Test that no common drop checks are taking place
+    expected = {
+        "a": TODICT_COMMON_DROP_VAL,
+        "b": TODICT_COMMON_DROP_VAL,
+        "c": TODICT_COMMON_DROP_VAL,
+        "d": TODICT_COMMON_DROP_VAL,
+        "e": TODICT_COMMON_DROP_VAL,
+    }
+    cut = ToDictTest(expected["a"], expected["b"], expected["c"], expected["d"], expected["e"])
+    result = cut.basic_dict5()
+    assert expected == result
+
+
+def test_basic_dict5_individual_drop_value():
+    # Test that no drop checks are taking place
+    expected = {
+        "a": TODICT_DROP_A_VAL,
+        "b": TODICT_DROP_B_VAL,
+        "c": TODICT_DROP_C_VAL,
+        "d": TODICT_DROP_D_VAL,
+        "e": TODICT_DROP_E_VAL,
+    }
+    cut = ToDictTest(expected["a"], expected["b"], expected["c"], expected["d"], expected["e"])
+    result = cut.basic_dict5()
+    assert expected == result
