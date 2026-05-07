c5728489853257f3b08afdf843b111eefa33b578
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-07 15:05:04 -0400

    Add basic functionality test for bounded single queue

---
+++ tests/ares-data-structures/src/bounded_queue_tests.cpp
@@ -0,0 +1,84 @@
+/**
+ * @file bounded_queue_tests.cpp
+ *
+ * @brief
+ *
+ * @date 5/7/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+#include <ares/data-structures/queue.hpp>
+#include <chrono>
+#include <gtest/gtest.h>
+#include <thread>
+#include <thread_utils.hpp>
+
+using namespace std::chrono_literals;
+
+TEST(queue_api, bounded_queue_no_overwrite_basic_functionality_single) {
+    ares::bounded_queue<int> cut;
+
+    int data[] = {
+        0xAAAA,
+        0xBBBB,
+        0xCCCC,
+    };
+    int val;
+
+    // Empty tests & size tests SINGLE
+    ASSERT_TRUE(cut.empty());
+    ASSERT_FALSE(cut.full());
+    ASSERT_EQ(cut.size(), 0);
+
+    cut.put(data[0]);
+    ASSERT_FALSE(cut.empty());
+    ASSERT_TRUE(cut.full());
+    ASSERT_EQ(cut.size(), 1);
+
+    cut.clear();
+    ASSERT_TRUE(cut.empty());
+    ASSERT_FALSE(cut.full());
+    ASSERT_EQ(cut.size(), 0);
+
+    cut.clear();
+    ASSERT_TRUE(cut.empty());
+    ASSERT_FALSE(cut.full());
+    ASSERT_EQ(cut.size(), 0);
+
+    // Empty & size multiple entries
+    ASSERT_NO_THROW(cut.put_nonblocking(data[0]));
+    ASSERT_THROW(cut.put_nonblocking(data[1]), ares::queue_exception);
+    ASSERT_EQ(cut.size(), 1);
+
+    cut.clear();
+
+    // Basic putting and getting single
+    ASSERT_NO_THROW(cut.put_nonblocking(data[0]));
+    ASSERT_EQ(cut.size(), 1);
+    ASSERT_NO_THROW(val = cut.get_nonblocking());
+    ASSERT_EQ(cut.size(), 0);
+    ASSERT_EQ(val, data[0]);
+
+    // Basic putting and getting multi
+    ASSERT_NO_THROW(cut.put_nonblocking(data[0]));
+    ASSERT_THROW(cut.put_nonblocking(data[1]), ares::queue_exception);
+    ASSERT_EQ(cut.size(), 1);
+    ASSERT_NO_THROW(val = cut.get_nonblocking());
+    ASSERT_EQ(cut.size(), 0);
+    ASSERT_EQ(val, data[0]);
+
+    ASSERT_NO_THROW(cut.put_nonblocking(data[1]));
+    ASSERT_THROW(cut.put_nonblocking(data[2]), ares::queue_exception);
+    ASSERT_EQ(cut.size(), 1);
+    ASSERT_NO_THROW(val = cut.get_nonblocking());
+    ASSERT_EQ(cut.size(), 0);
+    ASSERT_EQ(val, data[1]);
+
+    ASSERT_NO_THROW(cut.put_nonblocking(data[2]));
+    ASSERT_THROW(cut.put_nonblocking(data[0]), ares::queue_exception);
+    ASSERT_EQ(cut.size(), 1);
+    ASSERT_NO_THROW(val = cut.get_nonblocking());
+    ASSERT_EQ(cut.size(), 0);
+    ASSERT_EQ(val, data[2]);
+}
