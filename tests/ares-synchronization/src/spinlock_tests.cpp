703d2d5f126f9dec20bc1403b862e81cb9b6c19f
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-08 15:11:33 -0400

    Get synchronization set up

---
+++ tests/ares-synchronization/src/spinlock_tests.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file spinlock_tests.cpp
+ *
+ * @brief
+ *
+ * @date 5/8/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+#include <ares/synchronization/spinlock.hpp>
+#include <gtest/gtest.h>
