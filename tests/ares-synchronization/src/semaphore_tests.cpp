68ad632607491f2716253f70dcce506c4553d840
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-08 17:00:13 -0400

    Set up environment for semaphores

---
+++ tests/ares-synchronization/src/semaphore_tests.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file semaphore_tests.cpp
+ *
+ * @brief
+ *
+ * @date 5/8/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+#include <ares/synchronization/semaphore.hpp>
+#include <gtest/gtest.h>
+
+
