3edadcf3bfcb721340e2e2586750a39426f99692
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-11 11:39:51 -0400

    Write a basic operation test

--- tests/ares-synchronization/src/semaphore_tests.cpp
+++ tests/ares-synchronization/src/semaphore_tests.cpp
@@ -11,4 +11,28 @@
 #include <ares/synchronization/semaphore.hpp>
 #include <gtest/gtest.h>
 
+TEST(semaphore_api, basic_operation) {
+    ares::semaphore single_sem;
+    ares::semaphore<2> multi_sem;
 
+    ASSERT_EQ(single_sem.get_count(), 1);
+    ASSERT_EQ(multi_sem.get_count(), 2);
+
+    ASSERT_NO_THROW(single_sem.take(ares::no_wait));
+    ASSERT_NO_THROW(multi_sem.take(ares::no_wait));
+
+    ASSERT_EQ(single_sem.get_count(), 0);
+    ASSERT_EQ(multi_sem.get_count(), 1);
+
+    ASSERT_NO_THROW(multi_sem.take(ares::no_wait));
+    ASSERT_EQ(multi_sem.get_count(), 0);
+
+    ASSERT_NO_THROW(single_sem.give());
+    ASSERT_EQ(single_sem.get_count(), 1);
+
+    ASSERT_NO_THROW(multi_sem.give());
+    ASSERT_EQ(multi_sem.get_count(), 1);
+
+    ASSERT_NO_THROW(multi_sem.give());
+    ASSERT_EQ(multi_sem.get_count(), 2);
+}
