983d861ec007c4adba1602c76b3471d81132260d
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-11 11:27:18 -0400

    Refacor ares semaphore

--- include/ares/synchronization/semaphore.hpp
+++ include/ares/synchronization/semaphore.hpp
@@ -12,6 +12,7 @@
 #define ARES_COMMON_SEMAPHORE_HPP
 
 #include <ares/data-structures/queue.hpp>
+#include <ares/synchronization/internal/constants.hpp>
 #include <chrono>
 #include <exception>
 #include <string>
@@ -28,6 +29,30 @@ class timeout_exception : public std::exception {
      * @param msg The exception message.
      */
     explicit timeout_exception(const char *msg) : _msg(msg) {}
+
+    /**
+     * Retrieve the exception message.
+     * @return The exception message.
+     */
+    [[nodiscard]] const char *what() const noexcept override {
+        return _msg.c_str();
+    }
+
+  private:
+    std::string _msg;
+};
+
+/**
+ * @class semaphore_exception
+ * Semaphore exception class.
+ */
+class semaphore_exception : public std::exception {
+  public:
+    /**
+     * Constructor.
+     * @param msg The exception message.
+     */
+    explicit semaphore_exception(const char *msg) : _msg(msg) {}
 
     /**
      * Retrieve the exception message.
@@ -66,11 +91,6 @@ class semaphore {
     explicit semaphore(size_t init_cnt);
 
     /**
-     * Take the semaphore. Blocks indefinitely if it is not available.
-     */
-    void lock();
-
-    /**
      * Take the semaphore. Blocks for the given amount of time.
      *
      * @param[in] timeout The maximum time the semaphore is allowed to block.
@@ -81,33 +101,36 @@ class semaphore {
      * is the same as semaphore::lock().
      *
      * @throws timeout_exception if waiting period timeout out.
+     * @throws semaphore_exception if unable to take semaphore without blocking.
      */
-    void lock(std::chrono::milliseconds &timeout);
-
-    /**
-     * Same as semaphore::lock().
-     */
-    void take();
+    void lock(std::chrono::milliseconds timeout = forever);
 
     /**
      * Same as semaphore::lock().
      *
      * @param[in] timeout The maximum time the semaphore is allowed to block.
      *
-     * @note If timeout is std::chrono::milliseconds::zero(), then the operation
-     * becomes non-blocking.
-     * @note If timeout is std::chrono::milliseconds::max(), then the operation
-     * is the same as semaphore::take().
+     * @note If timeout is ares::no_wait then the operation becomes
+     * non-blocking.
+     * @note If timeout is ares::forever (default behavior), then the operation
+     * blocks until the semaphore becomes available.
+     *
+     * @throws timeout_exception if waiting period timeout out.
+     * @throws semaphore_exception if unable to take semaphore without blocking.
      */
-    void take(std::chrono::milliseconds &timeout);
+    void take(std::chrono::milliseconds timeout = forever);
 
     /**
      * Give the semaphore.
+     *
+     * @throws semaphore_exception if the semaphore count would overflow.
      */
     void unlock();
 
     /**
      * Same as semaphore::unlock().
+     *
+     * @throws semaphore_exception if the semaphore count would overflow.
      */
     void give();
 
@@ -125,7 +148,10 @@ class semaphore {
     size_t get_count();
 
   private:
-    bounded_queue<uint8_t, count, true> _sem;
+    bounded_queue<uint8_t, count> _sem;
+
+    void _blocking_lock();
+    void _nonblocking_lock(const std::chrono::milliseconds &timeout);
 };
 
 template <size_t count>
@@ -145,35 +171,30 @@ semaphore<count>::semaphore(size_t init_cnt) {
 }
 
 template <size_t count>
-void semaphore<count>::lock() {
-    _sem.get();
-}
-
-template <size_t count>
-void semaphore<count>::take() {
-    lock();
-}
-
-template <size_t count>
-void semaphore<count>::take(std::chrono::milliseconds &timeout) {
+void semaphore<count>::take(const std::chrono::milliseconds timeout) {
     lock(timeout);
 }
 
 template <size_t count>
-void semaphore<count>::lock(std::chrono::milliseconds &timeout) {
-    try {
-        _sem.get(timeout);
-    } catch (const queue_exception &exc) {
-        if (exc.reason() == queue_exception::QUEUE_TIMEOUT) {
-            throw timeout_exception("Semaphore `take` timed out");
-        }
-        throw;
+void semaphore<count>::lock(const std::chrono::milliseconds timeout) {
+    if (timeout == forever) {
+        _blocking_lock();
+    } else {
+        _nonblocking_lock(timeout);
     }
 }
 
 template <size_t count>
 void semaphore<count>::unlock() {
-    _sem.put_nonblocking(0);
+    try {
+        _sem.put_nonblocking(0);
+    } catch (const queue_exception &exc) {
+        if (exc.reason() == queue_exception::QUEUE_FULL) {
+            throw semaphore_exception(
+                "Cannot `give` semaphore more times than its permitted count");
+        }
+        throw;
+    }
 }
 
 template <size_t count>
@@ -185,11 +206,8 @@ template <size_t count>
 bool semaphore<count>::try_lock() {
     bool ret = true;
     try {
-        _sem.get_nonblocking();
-    } catch (const queue_exception &exc) {
-        if (exc.reason() != queue_exception::QUEUE_EMPTY) {
-            throw;
-        }
+        _nonblocking_lock(no_wait);
+    } catch (const semaphore_exception &) {
         ret = false;
     }
 
@@ -199,6 +217,28 @@ bool semaphore<count>::try_lock() {
 template <size_t count>
 size_t semaphore<count>::get_count() {
     return _sem.size();
+}
+
+template <size_t count>
+void semaphore<count>::_blocking_lock() {
+    _sem.get();
+}
+
+template <size_t count>
+void semaphore<count>::_nonblocking_lock(
+    const std::chrono::milliseconds &timeout) {
+    try {
+        _sem.get(timeout);
+    } catch (const queue_exception &exc) {
+        if (exc.reason() == queue_exception::QUEUE_TIMEOUT) {
+            throw timeout_exception("Semaphore `take` timed out");
+        }
+        if (exc.reason() == queue_exception::QUEUE_EMPTY) {
+            throw semaphore_exception(
+                "Unable to `take` semaphore without blocking");
+        }
+        throw;
+    }
 }
 } // namespace ares
 
