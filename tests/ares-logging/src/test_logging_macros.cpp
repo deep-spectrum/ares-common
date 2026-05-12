00649f66e99e858134ab2dcfaab4d50f283611cd
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-12 18:54:05 -0400

    Get next logging tests set up

---
+++ tests/ares-logging/src/test_logging_macros.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file test_logging_macros.cpp
+ *
+ * @brief
+ *
+ * @date 5/12/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+#include <ares/logging/log.hpp>
+#include <gtest/gtest.h>
