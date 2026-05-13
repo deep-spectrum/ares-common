46784d06465a2bffe0260bf76f950e5c0ca90ac3
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-13 11:34:14 -0400

    Write tests for logger declare macros

--- tests/ares-logging/src/test_logging_macros.cpp
+++ tests/ares-logging/src/test_logging_macros.cpp
@@ -10,3 +10,19 @@
 
 #include <ares/logging/log.hpp>
 #include <gtest/gtest.h>
+
+TEST(logger_api, register_macro_default_level) {
+    LOG_MODULE_REGISTER(default_level);
+
+    ASSERT_EQ(LOG_MODULE_CURRENT_LEVEL, DEFAULT_LOG_LEVEL);
+    ASSERT_EQ(LOG_MODULE_SAVED_LEVEL, DEFAULT_LOG_LEVEL);
+    ASSERT_EQ(LOG_MODULE_NAME, "default_level");
+}
+
+TEST(logger_api, register_macro_defined_level) {
+    LOG_MODULE_REGISTER(defined_level, LOG_LEVEL_ERROR);
+
+    ASSERT_EQ(LOG_MODULE_CURRENT_LEVEL, ares::Logger::LOG_LEVEL_ERROR);
+    ASSERT_EQ(LOG_MODULE_SAVED_LEVEL, ares::Logger::LOG_LEVEL_ERROR);
+    ASSERT_EQ(LOG_MODULE_NAME, "defined_level");
+}
