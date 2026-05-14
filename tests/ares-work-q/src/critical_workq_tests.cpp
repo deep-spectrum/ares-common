7d2260be72b3fb701ce9ac37cfcc8523baec8d29
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-14 15:27:57 -0400

    Set up tests for work and work queue

---
+++ tests/ares-work-q/src/critical_workq_tests.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file critical_workq_tests.cpp
+ *
+ * @brief
+ *
+ * @date 5/14/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
---
+++ tests/ares-work-q/src/work_tests.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file work_tests.cpp
+ *
+ * @brief
+ *
+ * @date 5/14/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
