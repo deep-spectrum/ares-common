1abb1d73fccbbb7aa5fab29a448063f6ae533b2f
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-05-15 14:33:34 -0400

    Write a test for unstarted queue

--- tests/ares-work-q/src/work_tests.cpp
+++ tests/ares-work-q/src/work_tests.cpp
@@ -7,3 +7,21 @@
  *
  * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
  */
+
+#include <ares/work-q/work_q.hpp>
+#include <gtest/gtest.h>
+
+static void counter_handler(ares::Work *work) {
+    // todo
+}
+
+TEST(work, unstarted) {
+    int rc;
+    ares::WorkQ not_started_q;
+
+    ares::Work work(counter_handler);
+    EXPECT_EQ(work.work_busy_get(), 0);
+
+    rc = not_started_q.submit(&work);
+    EXPECT_EQ(rc, -ENODEV);
+}
