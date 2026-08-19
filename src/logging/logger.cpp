15c237f0a7b777fa95aa8916942f05565cead85c
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-08-19 16:34:43 -0400

    Fix initialization issues

--- src/logging/logger.cpp
+++ src/logging/logger.cpp
@@ -19,7 +19,9 @@ constexpr const char *wrn_color = "\033[38;2;163;115;76m";
 constexpr const char *err_color = "\033[38;2;193;29;40m";
 constexpr const char *crit_color = "\033[38;2;117;80;123m";
 
-static std::map<std::string, std::shared_ptr<LoggerImpl>> loggers;
+__attribute__((init_priority(
+    101))) static std::map<std::string, std::shared_ptr<LoggerImpl>>
+    loggers;
 
 class LoggerImpl : std::enable_shared_from_this<LoggerImpl> {
   public:
